6691eb59ccf67680eae6595dee718deb80a273e4
Freakmiko <freakmiki@gmail.com>
2015-11-18 14:31:27 +0100

    changed: playerDetails.js, Reiterwechsel eingebaut und favoriten sichtbar/unsichtbar machen, Blatt 4 - m.kurila, s.brehm, p.geyer

--- ESEWMKD/Meilenstein 4/js/playerDetails.js
+++ ESEWMKD/Meilenstein 4/js/playerDetails.js
@@ -1,18 +1,50 @@
 var request = new XMLHttpRequest();
 var playerDataArray;
 
+function switchVisibility() {
+    for(var i = 0; i < playerDataArray.length; ++i) {
+        if(!playerDataArray[i].isFavorite)
+            if(playerDataArray[i].pRow.style.display == "table-row")
+                playerDataArray[i].pRow.style.display = "none";
+            else
+                playerDataArray[i].pRow.style.display = "table-row";
+    }
+}
+
+function switchTabs(reiter1, reiter2) {
+    if (reiter1.classList.contains("selected")) {
+        reiter2.classList.add("selected");
+        reiter1.classList.remove("selected");
+
+        switchVisibility();
+    }
+}
 function loadPlayers() {
-    request.open("GET", "http://localhost:63342/WAW/ESEWMKD/Meilenstein 4/js/data.json");
+    request.open("GET", "../js/data.json");
     request.onreadystatechange = requestHandler;
     request.send();
 
-    document.getElementsByClassName("tabellenReiterRechts")[0].addEventListener("click", function() {
+    var reiterRechts = document.getElementsByClassName("tabellenReiterRechts")[0];
+    var reiterLinks = document.getElementsByClassName("tabellenReiterLinks")[0];
+
+    reiterRechts.addEventListener("click", function() {
+        switchTabs(reiterLinks, reiterRechts);
+    });
+
+    reiterLinks.addEventListener("click", function() {
+        switchTabs(reiterRechts, reiterLinks);
     });
 }
 
 function loadTable() {
     for(var i = 0; i < playerDataArray.length; ++i) {
         var tableRow = document.getElementById("spielerTabelle").insertRow(i+1);
+
+        // fuege dem style des tablerows das display hinzu damit
+        // es bei dem reiter wechsel verfuegbar ist
+        tableRow.style.display = "table-row";
+        playerDataArray[i].pRow = tableRow;
+
         tableRow.insertCell(0).innerHTML = playerDataArray[i].firstname + " " + playerDataArray[i].surname;
         tableRow.insertCell(1).innerHTML = playerDataArray[i].team;
         tableRow.insertCell(2).innerHTML = playerDataArray[i].headcoach;
