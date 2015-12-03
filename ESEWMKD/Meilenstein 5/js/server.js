1abe936c4c66f9b4a0a26df43a81073f908a66ab
Michael Kurila <freakmiki+smfgit@gmail.com>
2015-12-03 18:21:56 +0100

    changed: server.js, Spieler werden nun in die Datei form.txt geschrieben, Blatt 5, m.kurila

--- ESEWMKD/Meilenstein 5/js/server.js
+++ ESEWMKD/Meilenstein 5/js/server.js
@@ -5,18 +5,22 @@
 
 var http = require("http");
 var url = require("url");
+var fs = require("fs");
 
 http.createServer(function(request, respone){
 
+    if (request.url == "/favicon.ico")
+        return;
+
     console.log("user connected");
 
+    // extract the arguments from the url so that we can use them and write them to the file
     var url_parts = url.parse(request.url, true);
     var query = url_parts.query;
-
-    console.log(query);
+    fs.appendFile("form.txt", query["vorname"] + " " + query["name"] + ", "
+        + query["jahr"] + ", " + query["hcoach"] + ", " + query["acoach"] + ", " + query["position"]
+        + ", " + query["number"] + "\n");
 
     respone.writeHead(200, {"Content-Type":"text/plain"});
     respone.end("Sie haben sich erfolgreich auf den WebServer mir der Url 127.0.0.1:1337 verbunden");
-
-
 }).listen(1337, "127.0.0.1");
